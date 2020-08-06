17e21e772c1f20467982e260663f8e98128dc7aa
zaaack <zaaack@qq.com>
2020-08-06 21:56:18 +0800

    fix: using js timers

--- src/popup/utils/index.ts
+++ src/popup/utils/index.ts
@@ -3,57 +3,58 @@ import constate from 'constate'
 import { useState } from 'react'
 // import type { AlarmManager } from '../../background/index'
 import { Alarm, alarmKv, Duration } from 'background/kv'
-import { createTimer } from 'background'
+import { AlarmManager } from 'background'
+// import { createTimer } from 'background'
 export const history = createHashHistory()
 
-// export function getAlarmManager(): AlarmManager {
-//   let bg = chrome.extension.getBackgroundPage() as any
-//   return bg['alarmManager']
-// }
 
 export interface Actions {
   alarms: Alarm[]
   fetchAlarms: () => void
 }
 
-// function useAlarm() {
-//   let [alarms, setAlarms] = useState<Alarm[]>(getAlarmManager().alarms)
-//   function fetch() {
-//     setAlarms(getAlarmManager().alarms)
-//   }
-//   return {
-//     alarms,
-//     fetch,
-//     remove(a: Alarm) {
-//       getAlarmManager().remove(a)
-//       fetch()
-//     },
-//     save(a: Alarm) {
-//       getAlarmManager().save(a)
-//       fetch()
-//     },
-//   }
-// }
-
+export function getAlarmManager(): AlarmManager {
+  let bg = chrome.extension.getBackgroundPage() as any
+  return bg['alarmManager']
+}
 function useAlarm() {
-  let [alarms, setAlarms] = useState<Alarm[]>(alarmKv.getAll())
+  let [alarms, setAlarms] = useState<Alarm[]>(getAlarmManager().alarms)
   function fetch() {
-    setAlarms(alarmKv.getAll())
+    setAlarms(getAlarmManager().alarms)
   }
   return {
     alarms,
     fetch,
     remove(a: Alarm) {
-      alarmKv.remove(a)
-      chrome.alarms.clear(a.id)
+      getAlarmManager().remove(a)
       fetch()
     },
     save(a: Alarm) {
-      alarmKv.save(a)
-      createTimer(a)
+      getAlarmManager().save(a)
       fetch()
     },
   }
 }
+
+// function useAlarm() {
+//   let [alarms, setAlarms] = useState<Alarm[]>(alarmKv.getAll())
+//   function fetch() {
+//     setAlarms(alarmKv.getAll())
+//   }
+//   return {
+//     alarms,
+//     fetch,
+//     remove(a: Alarm) {
+//       alarmKv.remove(a)
+//       chrome.alarms.clear(a.id)
+//       fetch()
+//     },
+//     save(a: Alarm) {
+//       alarmKv.save(a)
+//       createTimer(a)
+//       fetch()
+//     },
+//   }
+// }
 
 export const [AlarmProvider, useAlarmContext] = constate(useAlarm)
